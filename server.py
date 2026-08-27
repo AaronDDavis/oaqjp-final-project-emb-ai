5bc3f1e1f74c94a388a5bba136f210b6cefd2783
AaronDDavis <107917731+AaronDDavis@users.noreply.github.com>
2026-08-27 22:16:46 +0800

    Task 6 revised

--- server.py
+++ server.py
@@ -3,9 +3,9 @@ from emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
-@app.route('/emotionDetector')
-def run_detector():
-    emotions = emotion_detector()
+@app.route('/emotionDetector?textToAnalyze=<string:prompt>')
+def run_detector(prompt):
+    emotions = emotion_detector(prompt)
     return f"For the given statement, the system response is 'anger': {emotions['anger']}, 'disgust': {emotions['disgust']}, 'fear': {emotions['fear']}, 'joy': {emotions['joy']} and 'sadness': {emotions['sadness']}. The dominant emotion is {emotions['dominant_emotion']}."
 
 if __name__ == "__main__":
