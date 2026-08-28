1d5f20fc75708a3c14b1ae9720c082bdd39485a7
AaronDDavis <107917731+AaronDDavis@users.noreply.github.com>
2026-08-28 22:51:21 +0800

    Bug fix

--- emotion_detection.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def emotion_detector(text_to_analyze):
-    response = requests.post(url="https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict", headers={"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}, json={"raw_document": { "text": text_to_analyze }});
-    emotions = response.json()["emotionPredictions"][0]["emotion"]
-    dominant_emotion_val = max(emotions.values())
-    emotions["dominant_emotion"] = [key for (key, value) in emotions.items() if value == dominant_emotion_val][0]
-    return emotions
